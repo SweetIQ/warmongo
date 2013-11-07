1192b21aa36b4fd0a3811a02be3ef69c89733266
Rob Britton <rob@sweetiq.com>
2013-11-07 15:51:32 -0500

    Bumped version number.

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ def parse_requirements():
 
 setuptools.setup(
     name='warmongo',
-    version='0.4.3',
+    version='0.4.4',
     description='JSON-Schema-based ORM for MongoDB',
     author='Rob Britton',
     author_email='rob@robbritton.com',
